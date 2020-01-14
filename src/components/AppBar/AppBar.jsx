a457091dd082fde90b492bc4afb7c8a136dd3f19
Giorgos Petrou <gpet88@gmail.com>
2020-01-15 00:15:28 +0200

    Adds shopping cart modal

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AppBar.css";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 const AppBar = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <Navbar className="navbar" bg="light" variant="light">
       <Navbar.Brand href="#home">Jersey Store</Navbar.Brand>
-      <Button className="cartButton" variant="info">
+      <Button className="cartButton" variant="info" onClick={handleShow}>
         <img
           alt=""
           src="../../images/icon-cart.png"
@@ -16,6 +22,17 @@ const AppBar = () => {
           className="cartImage"
         />
       </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Your Shopping Cart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Items in the cart!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="dark" onClick={handleClose}>
+            Checkout
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Navbar>
   );
 };
